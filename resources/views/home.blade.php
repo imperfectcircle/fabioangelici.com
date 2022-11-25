9e28581ab3536589d54151df56ffd2b646797245
Fabio Angelici <fabio.angelici@outlook.it>
2022-11-25 22:48:12 +0100

    Creato progetto con tailwind e fortify

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html class="scrollbar-thin scrollbar-track-slate-100 scrollbar-thumb-slate-500" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="w-full max-h-fit bg-slate-600">
+        <i class="fa-solid fa-house">Prova</i>
+    </div>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '769px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Josefin Sans', 'sans-serif'],
+                alata: ['Alata'],
+            },
+        },
+    },
+    plugins: [
+        require('tailwind-scrollbar'),
+    ],
+};
