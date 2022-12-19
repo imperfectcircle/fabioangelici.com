5593bd89e3b4a8ddf6a9a5c063fb6115d1262874
Fabio Angelici <fabio.angelici@outlook.it>
2022-12-19 16:32:03 +0100

    Aggiunta pagina errore 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+<x-layout :title="'Fabio Angelici | Pagina non trovata'"
+    :description="'Sembra che quello che stai cercando non si trovi qui'"
+    :ogUrl="'https://www.fabioangelici.com/404'"
+    :ogDescription="'Sembra che quello che stai cercando non si trovi qui'"
+    :ogTitle="'Fabio Angelici | Pagina non trovata'"
+    :canonical="'https://fabioangelici.com/404'"
+>
+<section id="hero" class="notfound flex flex-col justify-center text-white">
+    <div class="text-center md:text-left md:ml-24">
+        <h1 class="text-6xl">Opss... A quanto pare quello che stavi cercando non si trova qui.</h1>
+        
+    </div>
+</section>
+</x-layout>
