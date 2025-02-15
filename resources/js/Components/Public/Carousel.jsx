60295e934a6d76cd3b1d0af69efeb58f73c774e9
Fabio Angelici <fabio.angelici@outlook.it>
2025-02-15 21:51:07 +0100

    Modificato colore bg controlli carosello

--- resources/js/Components/Public/Carousel.jsx
+++ resources/js/Components/Public/Carousel.jsx
@@ -127,7 +127,7 @@ const Slide = ({ slide, index, current, handleSlideClick }) => {
 const CarouselControl = ({ type, title, handleClick }) => {
   return (
     <button
-      className={`w-10 h-10 flex items-center mx-2 justify-center bg-neutral-200 dark:bg-neutral-800 border-3 border-transparent rounded-full focus:border-[#6D64F7] focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200 ${
+      className={`w-10 h-10 flex items-center mx-2 justify-center bg-neutral-200 dark:bg-neutral-800 border-3 border-transparent rounded-full focus:border-[#6D64F7] focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200 bg-dark ${
         type === "previous" ? "rotate-180" : ""
       }`}
       title={title}
